c5d8cb4adf0c9ea7a07e50961a08eacc1a63322a
annieleonia <annie.onnered@gmail.com>
2018-11-21 18:22:47 +0100

    Add support for Ladok 3.0

--- scripts.js
+++ scripts.js
@@ -1,16 +1,36 @@
-  function CalculateAvgGrade(){
+function CalculateAvgGrade() {
+  lines = document.getElementById("textarea").value.split("\n");
+  totalPoints = 0;
+  countingGrade = 0;
 
-    lines = document.getElementById("textarea").value.split('\n')
-    totalPoints = 0
-    countingGrade = 0
+  for (var i = 0; i < lines.length; i++) {
+    res = lines[i].split("\t");
+    if (!isNaN(res[5])) {
+      totalPoints += res[3] * res[5];
+      countingGrade += parseFloat(res[3]);
+    }
+    avgGrade = (totalPoints / countingGrade).toFixed(2);
+    document.getElementById("yourGrade").innerHTML = avgGrade;
+  }
+}
 
-    for(var i = 0;i < lines.length;i++){
-      res = lines[i].split('\t')
-      if(!isNaN(res[5])){
-        totalPoints += res[3] * res[5]
-        countingGrade += parseFloat(res[3])
+function CalculateAvgGrade3() {
+  lines = document.getElementById("textarea").value.split("\n");
+  totalPoints = 0;
+  countingGrade = 0;
+
+  for (var i = 0; i < lines.length; i++) {
+    if (lines[i]) {
+      pointsStr = lines[i].split(" | ")[1].replace(",", ".");
+      pointsFlo = parseFloat(pointsStr.substring(0, pointsStr.length - 3));
+      grade = lines[i + 1].substring(7, 8);
+      if (!isNaN(grade)) {
+        countingGrade += pointsFlo * grade;
+        totalPoints += pointsFlo;
       }
-      avgGrade = (totalPoints / countingGrade).toFixed(2);
-      document.getElementById("yourGrade").innerHTML = avgGrade
+      i++;
     }
   }
+  avgGrade = (countingGrade / totalPoints).toFixed(2);
+  document.getElementById("yourGrade").innerHTML = avgGrade;
+}
